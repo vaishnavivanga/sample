95ddeeafdb4f341bc71d076a41f45101229cc88a
acer <acer@boss>
2018-07-07 15:21:00 +0530

    hello

--- main.js
+++ main.js
@@ -15,5 +15,51 @@ getfile("data.json",function(text)
 {
 var data = JSON.parse(text);
 console.log(data);
+career(data.career);
+education(data.education);
+skills(data.skills);
 }
 )
+var child2=document.querySelector(".childtwo");
+function career(careerInfo){
+  var careerHeading=document.createElement("h3");
+    child2.appendChild(careerHeading);
+    careerHeading.textContent="career objective";
+
+    var careerHr=document.createElement("hr");
+    child2.appendChild(careerHr);
+
+    var ch=document.createElement("p");
+    ch.textContent=careerInfo.info;
+    child2.appendChild(ch);
+  }
+  function education(edu){
+    var eduHeading=document.createElement("h3");
+      eduHeading.textContent="educational qualifications";
+      child2.appendChild(eduHeading);
+
+      var careerHr=document.createElement("hr");
+      child2.appendChild(careerHr);
+
+var eduTable=document.createElement("table");
+eduTable.border="1";
+var tr1="<tr><td>degree</td><td>institution</td><td>data</td></tr>"
+//eduTable.innerHTML=tr1;
+//tr1.style.color="#fff";
+eduTable.innerHTML=tr1;
+var tr2="";
+for(var i=0;i<edu.length; i++){
+  tr2+="<tr><td>"+edu[i].degree+"</td><td>"+edu[i].institution+"</td><td>"+edu[i].data+"</td></tr>";
+}
+eduTable.innerHTML=tr1+tr2;
+child2.appendChild(eduTable);
+}
+function skills(skil){
+  var ul=document.createElement("ul");
+  child2.appendChild(ul);
+  for(var i=0;i<skil.length;i++){
+    var li=document.createElement("li");
+    li.textContent=skil[i].title+":"+skil[i].content;
+    ul.appendChild(li);
+  }
+}
